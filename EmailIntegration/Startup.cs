52349d7a69496e55de79e983deb864d8b3583c56
priteymehta <priteymehta235@gmail.com>
2018-12-18 18:27:26 +0530

    initial commit

---
+++ EmailIntegration/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmailIntegration.Startup))]
+namespace EmailIntegration
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
